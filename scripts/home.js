d393f2afb55773cbf65ec30e0fd677ab73ff3ce1
domkay <domkayombo@gmail.com>
2025-01-21 11:36:34 +0200

    new

--- scripts/home.js
+++ scripts/home.js
@@ -5,3 +5,13 @@ const lastModified = document.lastModified;
 
 // Display the last modified date in the paragraph
 document.getElementById("lastModified").textContent += lastModified;
+
+// Hmaburger menu
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
