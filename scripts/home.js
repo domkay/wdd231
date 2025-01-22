f5464d11d07e73ac3b0cb4ede919e1c93da78528
domkay <domkayombo@gmail.com>
2025-01-22 23:46:05 +0200

    new

--- scripts/home.js
+++ scripts/home.js
@@ -42,3 +42,11 @@ document.addEventListener("DOMContentLoaded", () => {
   // Initial check
   handleResize();
 });
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+// Get the last modified date of the HTML document
+const lastModified = document.lastModified;
+
+// Display the last modified date in the paragraph
+document.getElementById("lastModified").textContent += lastModified;
